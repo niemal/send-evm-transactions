61492f92539fcabfc9833f4740f39f024722d240
niemal <niemal@gmx.com>
2024-05-24 05:01:19 +0300

    fix: add multiple RPC nodes and sleep inbetween iterations

--- index.js
+++ index.js
@@ -2,60 +2,74 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 
 const PRIVATE_KEY = "MY_PRIVATE_KEY";
-const RPC_URL = "https://arbitrum.llamarpc.com";
 const recipientAddress = "0xef4dea791c782c0b82f33d2df41d4188016d6494";
 const numTransactions = 100;
+const iterations = 50;
 const amountToSend = "0.00006"; // 27 kelp
 
+const RPC_URLS = [
+  "https://arbitrum.llamarpc.com",
+  "https://arb-pokt.nodies.app",
+  "https://arbitrum.drpc.org",
+  "https://endpoints.omniatech.io/v1/arbitrum/one/public",
+];
+
+function getRandomRPCUrl() {
+  return RPC_URLS[Math.floor(Math.random() * RPC_URLS.length)];
+}
+
 async function sendTransactions() {
-  const provider = new HDWalletProvider(PRIVATE_KEY, RPC_URL);
-  const web3 = new Web3(provider);
+  for (let i = 0; i < iterations; i++) {
+    const promiseArray = [];
 
-  const accounts = await web3.eth.getAccounts();
-  const senderAddress = accounts[0];
+    for (let i = 0; i < Math.floor(numTransactions / iterations); i++) {
+      const provider = new HDWalletProvider(PRIVATE_KEY, getRandomRPCUrl());
+      const web3 = new Web3(provider);
 
-  console.log(`Sender address: ${senderAddress}`);
-  console.log(`Recipient address: ${recipientAddress}`);
-  console.log(`Number of transactions: ${numTransactions}`);
-  console.log(`Amount to send per transaction: ${amountToSend} ETH`);
+      const accounts = await web3.eth.getAccounts();
+      const senderAddress = accounts[0];
 
-  const promiseArray = [];
-  const value = web3.utils.toWei(amountToSend, "ether");
-  const gasPrice = await web3.eth.getGasPrice();
-  const gasLimit = await web3.eth.estimateGas({
-    from: senderAddress,
-    to: recipientAddress,
-    value,
-  });
+      const value = web3.utils.toWei(amountToSend, "ether");
+      const gasPrice = await web3.eth.getGasPrice();
+      const gasLimit = await web3.eth.estimateGas({
+        from: senderAddress,
+        to: recipientAddress,
+        value,
+      });
 
-  const transaction = {
-    from: senderAddress,
-    to: recipientAddress,
-    value,
-    gasPrice,
-    gas: gasLimit,
-  };
+      const transaction = {
+        from: senderAddress,
+        to: recipientAddress,
+        value,
+        gasPrice,
+        gas: gasLimit,
+      };
 
-  for (let i = 0; i < numTransactions; i++) {
-    promiseArray.push(async () => {
-      try {
-        const receipt = await web3.eth.sendTransaction(transaction);
-        console.log(`Transaction ${i + 1} sent: ${receipt.transactionHash}`);
-        return true;
-      } catch (error) {
-        console.error(`Error sending transaction ${i + 1}:`, error);
-        return false;
-      }
-    });
+      promiseArray.push(async () => {
+        try {
+          const receipt = await web3.eth.sendTransaction(transaction);
+          console.log(`Transaction ${i + 1} sent: ${receipt.transactionHash}`);
+          provider.engine.stop();
+          return true;
+        } catch (error) {
+          console.error(`Error sending transaction ${i + 1}:`, error);
+          provider.engine.stop();
+          return false;
+        }
+      });
+    }
+    const results = await Promise.all(promiseArray.map((f) => f()));
+    const successfulTransactions = results.filter((result) => result).length;
+
+    console.log(
+      `Sent ${successfulTransactions} out of ${numTransactions} transactions :: iteration ${
+        i + 1
+      }/${iterations}`
+    );
   }
 
-  const results = await Promise.all(promiseArray.map((f) => f()));
-  const successfulTransactions = results.filter((result) => result).length;
-
-  console.log(
-    `Sent ${successfulTransactions} out of ${numTransactions} transactions.`
-  );
-  provider.engine.stop();
+  // Wait 10 seconds.
+  await new Promise((resolve) => setTimeout(resolve, 10000));
 }
 
 async function main() {
